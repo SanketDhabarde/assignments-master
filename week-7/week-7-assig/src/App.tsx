1a9d79abfb08591c63eca61854fdbf1e11d8812c
SanketDhabarde <sanketdhabarde13@gmail.com>
2024-02-17 15:51:21 +0530

    refator: received props in card

--- week-7/week-7-assig/src/App.tsx
+++ week-7/week-7-assig/src/App.tsx
@@ -4,7 +4,15 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-      <Card />
+      <Card
+        name="Rita corrida"
+        age={32}
+        place="London"
+        followers={83}
+        likes={803}
+        posts={1.4}
+        image="https://a.storyblok.com/f/191576/1200x800/faa88c639f/round_profil_picture_before_.webp"
+      />
     </>
   );
 }
--- week-7/week-7-assig/src/components/Card.tsx
+++ week-7/week-7-assig/src/components/Card.tsx
@@ -1,32 +1,42 @@
-function Card() {
+interface CradProps {
+  name: string;
+  age: number;
+  image: string;
+  place: string;
+  followers: number;
+  likes: number;
+  posts: number;
+}
+
+function Card({ name, age, image, place, followers, likes, posts }: CradProps) {
   return (
     <div className="w-80 border-2 border-black-50">
       <div className="relative bg-green-200 w-full h-24">
         <img
           className="w-24 h-24 rounded-full object-cover absolute left-28 top-10"
-          src="https://a.storyblok.com/f/191576/1200x800/faa88c639f/round_profil_picture_before_.webp"
+          src={image}
           alt="profile"
         />
       </div>
       <div className="mt-10 text-center">
         <div className="flex justify-center items-center gap-1">
-          <h2 className="font-bold text-lg">Rita corrida</h2>
-          <span className="text-gray-500">32</span>
+          <h2 className="font-bold text-lg">{name}</h2>
+          <span className="text-gray-500">{age}</span>
         </div>
-        <p className="text-gray-500">London</p>
+        <p className="text-gray-500">{place}</p>
       </div>
       <hr className="mt-4" />
       <div className="flex justify-between p-4">
         <div>
-          <p className="font-bold">80k</p>
+          <p className="font-bold">{followers}K</p>
           <span className="text-gray-500">Followers</span>
         </div>
         <div>
-          <p className="font-bold">803k</p>
+          <p className="font-bold">{likes}K</p>
           <span className="text-gray-500">Likes</span>
         </div>
         <div>
-          <p className="font-bold">1.4k</p>
+          <p className="font-bold">{posts}K</p>
           <span className="text-gray-500">Posts</span>
         </div>
       </div>
